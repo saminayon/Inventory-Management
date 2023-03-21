cf662a19a9fbf5a3232dd45db2a6c1044db18790
Samin Yaser Ayon <ayon230@gmail.com>
2023-03-21 17:00:55 +0600

    Update main.js

--- main.js
+++ main.js
@@ -1,10 +1,8 @@
-const ProductName='Potato'
-const productprice = 30;
-const   productquantity = 3;
+const productinfo=['Potato',30,3]
 /*
 */
-console.log(`Product Name- ${ProductName}
-Product Price- ${productprice}
-Product Quantity ${productquantity}`
+console.log(`Product Name- ${productinfo[0]}
+Product Price- ${productinfo[1]}
+Product Quantity ${productinfo[2]}`
 )
 
