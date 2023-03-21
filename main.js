6e58496c142110beedc3afe67f96585ea1f9171f
Samin Yaser Ayon <ayon230@gmail.com>
2023-03-21 16:23:53 +0600

    Practic

---
+++ main.js
@@ -0,0 +1,10 @@
+const ProductName='Potato'
+const productprice = 30;
+const   productquantity = 3;
+/*
+*/
+console.log(`Product Name- ${ProductName}
+Product Price- ${productprice}
+Product Quantity ${productquantity}`
+)
+
