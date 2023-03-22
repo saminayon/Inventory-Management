591e5d58ef7d91b71c11fa5332548ef9e780bb92
Samin Yaser Ayon <ayon230@gmail.com>
2023-03-22 10:43:30 +0600

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
-const productinfo=['Potato',30,3]
-/*
-*/
-console.log(`Product Name- ${productinfo[0]}
+//Use of Array
+//const productinfo=['Potato',30,3]
+
+/*console.log(`Product Name- ${productinfo[0]}
 Product Price- ${productinfo[1]}
 Product Quantity ${productinfo[2]}`
-)
+)*/
+
+//use of object
+
 
