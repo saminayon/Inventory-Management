3b4d0a64a3c90a83d5cb40154030e17b38fa14ee
Samin Yaser Ayon <ayon230@gmail.com>
2023-03-25 12:46:59 +0600

    Update main.js

--- main.js
+++ main.js
@@ -1,29 +1,17 @@
- const product=[
-    {
-        productName: 'Potato',
-        productPrice: 30,
-        productQuantity: 3,},
-    {
-        productName: 'Tomato',
-        productPrice: 50, 
-        productQuantity: 10 ,}
-];
-function showproductinfo(product, productno)
-    {console.log(product)
-        const[product1, product2]=product;
-        //console.log(product1)
-        //console.log(product2)
+//For Loop
+const productinfoarray=['Potato',30,3]
+    for (let i=0; i<productinfoarray.length; i+=1){
+        console.log(productinfoarray[i])
+    }
 
-    return`${productno} Product Name- ${product1.productName},
-                        Product Price- ${product1.productPrice} Tk./kg,
-                        Product Quantity ${product1.productQuantity} kg`
-}
-const productoutput= showproductinfo(product,'1.');
-console.log(productoutput);
+//While Loop
+let i=0;
+while(i<productinfoarray.length){
+    console.log(productinfoarray[i]);
+    i++;
+}   
 
-for(let i=0; i<10; i=+1){
-    console.log(i); 
-}
+
 
 
  
