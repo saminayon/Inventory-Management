462425757e8f2846b191b52eaa74ef33cf134ef6
Samin Yaser Ayon <ayon230@gmail.com>
2023-03-25 11:08:05 +0600

    V-14 for better structure

--- main.js
+++ main.js
@@ -95,8 +95,8 @@ console.log(product1)
 console.log(product2)
 */
 
-//Function Vs Method
-const productInfoObj1={
+//Function Vs Method video-13
+/*const productInfoObj1={
     productName: 'Potato',
     productPrice: 30,
     productQuantity: 3,
@@ -109,3 +109,33 @@ const productInfoObj1={
      }
 }
      console.log(productInfoObj1.showinfo())
+     */
+
+//video-14
+const product=[
+    {
+        productName: 'Potato',
+        productPrice: 30,
+        productQuantity: 3,},
+    {
+        productName: 'Tomato',
+        productPrice: 50,
+        productQuantity: 10 ,}
+]
+
+function showproductinfo(product, productno)
+    {console.log(product)
+        const[product1, product2]=product;
+        //console.log(product1)
+        //console.log(product2)
+
+    return`${productno} Product Name- ${product1.productName},
+                        Product Price- ${product1.productPrice} Tk./kg,
+                        Product Quantity ${product1.productQuantity} kg`
+}
+const productoutput= showproductinfo(product,'1.')
+console.log(productoutput)
+
+
+
+ 
